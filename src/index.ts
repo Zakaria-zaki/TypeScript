70b763cd26f0f2b9058037ae0a111ffbcb46c589
zattaoui <zakaria.attaoui@midgard-ai.com>
2023-01-27 01:21:56 +0100

    class generique

--- src/index.ts
+++ src/index.ts
@@ -35,5 +35,28 @@ const newCar: Car = {
 
 const myFruits = addItemToCollection(newFruit, [])
 const myCars = addItemToCollection(newCar, [])
-console.log(myFruits[0].price)
-console.log(myCars[0].speed)
+// console.log(myFruits[0].price)
+// console.log(myCars[0].speed)
+
+
+class Stack<T> {
+    items: T[] = [];
+
+
+    push(item: T): void {
+        this.items.push(item);
+    }
+
+    pop(): T {
+        return this.items.pop();
+    }
+
+    display(): void{
+        console.log(this.items);
+    }
+}
+
+const newStack = new Stack<number>();
+newStack.push(1);
+newStack.display();
+
