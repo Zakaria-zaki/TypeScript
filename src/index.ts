6aed4fca79b53b609f7b782e1f4b20b2c24b0dfd
zattaoui <zakaria.attaoui@midgard-ai.com>
2023-01-27 01:07:33 +0100

    Les types generique

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,39 @@
-interface User {
-    name: string;
-    age: number;
-    adresse?: string;
-    [propName: string]: any
+interface Fruit {
+    name: string,
+    price: number,
 }
 
-const user: User = {
-    name: "zakaria",
-    age: 24
+interface Car {
+    speed: number
 }
+
+function addItemToCollection<T>(item: T, collection: T[]): T[] {
+    return [...collection, item];
+}
+
+interface AddItemFunction {
+    <T>(item: T, collection: T[]): T[]
+}
+
+const addItemTo: AddItemFunction = <T>(
+    item: T, 
+    collection: T[]
+    ): T[] => {
+    return [...collection, item]
+}
+
+
+
+const newFruit: Fruit = {
+    name: 'banane',
+    price: 1
+}
+
+const newCar: Car = {
+    speed: 44
+}
+
+const myFruits = addItemToCollection(newFruit, [])
+const myCars = addItemToCollection(newCar, [])
+console.log(myFruits[0].price)
+console.log(myCars[0].speed)
